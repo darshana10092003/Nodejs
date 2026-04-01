a6cb06c30e28a88dff8664122647d9c1c77f0ae9
darshana10092003 <dapatidar@trueigtech.com>
2026-04-01 10:33:59 +0530

    initial commit

--- errorhandling.js
+++ errorhandling.js
@@ -1,16 +1,47 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   throw new Error("Something went wrong!");
+// });
+
+// //custom error handling
+
+// app.use((err, req, res, next)=>{
+//     console.log(err);
+//     res.status(500).json({message:"Oops! something went wrong"});
+// });
+
+// app.listen(3008, () => {
+//   console.log("Server running on port 3008");
+// });
+
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  throw new Error("Something went wrong!");
+
+//route
+app.get("/error",(req,res,next)=>{
+  const err=new Error("something went wrong");
+  next(err);
 });
 
-//custom error handling
+app.get('/user',(req,res,next)=>{
+  const user =null;
 
-app.use((err, req, res, next)=>{
-    console.log(err);
-    res.status(500).json({message:"Oops! something went wrong"});
-});
+  if(!user){
+    const err=new Error("user not found");
+    err.status=404;
+    return next(err);
+  }
+})
+
+//error middleware
+app.use((err,req,res,next)=>{
+  console.log(err.message);
+  res.status(500).send("Internal server error");
+})
 
 app.listen(3008, () => {
   console.log("Server running on port 3008");
